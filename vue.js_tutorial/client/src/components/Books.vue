81f2154e5ed0e2c5347b86698a944bde1d048881
MasaKu <masaki_kuyama@yahoo.co.jp>
2019-11-04 11:37:58 +0900

    docker file config

--- vue.js_tutorial/client/src/components/Books.vue
+++ vue.js_tutorial/client/src/components/Books.vue
@@ -6,6 +6,7 @@
         <hr />
         <br />
         <br />
+        <alert :message="message" v-if="showMessage"></alert>
         <button type="button" class="btn btn-success btn-sm" v-b-modal.book-modal>Add Book</button>
         <br />
         <br />
@@ -28,8 +29,17 @@
               </td>
               <td>
                 <div class="btn-group" role="group">
-                  <button type="button" class="btn btn-warning btn-sm">Update</button>
-                  <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                  <button
+                    type="button"
+                    class="btn btn-warning btn-sm"
+                    v-b-modal.book-update-modal
+                    @click="editBook(book)"
+                  >Update</button>
+                  <button
+                    type="button"
+                    class="btn btn-danger btn-sm"
+                    @click="onDeleteBook(book)"
+                  >Delete</button>
                 </div>
               </td>
             </tr>
@@ -68,11 +78,48 @@
         </b-button-group>
       </b-form>
     </b-modal>
+
+    <b-modal ref="editBookModal" id="book-update-modal" title="Update" hide-footer>
+      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+        <b-form-group id="form-title-edit-group" label="Title:" label-for="form-title-edit-input">
+          <b-form-input
+            id="form-title-edit-input"
+            type="text"
+            v-model="editForm.title"
+            required
+            placeholder="Enter title"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="form-author-edit-group"
+          label="Author:"
+          label-for="form-author-edit-input"
+        >
+          <b-form-input
+            id="form-author-edit-input"
+            type="text"
+            v-model="editForm.author"
+            required
+            placeholder="Enter author"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group id="form-read-edit-group">
+          <b-form-checkbox-group v-model="editForm.read" id="form-checks">
+            <b-form-checkbox value="true">Read?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Update</b-button>
+          <b-button type="reset" variant="danger">Cancel</b-button>
+        </b-button-group>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Alert from './Alert.vue';
 
 export default {
   data() {
@@ -83,7 +130,18 @@ export default {
         author: '',
         read: [],
       },
+      editForm: {
+        id: '',
+        title: '',
+        author: '',
+        read: [],
+      },
+      message: '',
+      showMessage: false,
     };
+  },
+  components: {
+    alert: Alert,
   },
   methods: {
     getBooks() {
@@ -104,6 +162,8 @@ export default {
         .post(path, payload)
         .then(() => {
           this.getBooks();
+          this.message = 'Book added!';
+          this.showMessage = true;
         })
         .catch(error => {
           // eslint-disable-next-line
@@ -115,6 +175,10 @@ export default {
       this.addBookForm.title = '';
       this.addBookForm.author = '';
       this.addBookForm.read = [];
+      this.editForm.id = '';
+      this.editForm.title = '';
+      this.editForm.author = '';
+      this.editForm.read = [];
     },
     onSubmit(evt) {
       evt.preventDefault();
@@ -133,6 +197,60 @@ export default {
       evt.preventDefault();
       this.$refs.addBookModal.hide();
       this.initForm();
+    },
+    editBook(book) {
+      this.editForm = book;
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      let read = false;
+      if (this.editForm.read[0]) read = true;
+      const payload = {
+        title: this.editForm.title,
+        author: this.editForm.author,
+        read,
+      };
+      this.updateBook(payload, this.editForm.id);
+    },
+    updateBook(payload, bookID) {
+      const path = `http://localhost:5000/books/${bookID}`;
+      axios
+        .put(path, payload)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book updated!';
+          this.showMessage = true;
+        })
+        .catch(error => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      this.initForm();
+      this.getBooks(); // why?
+    },
+    removeBook(bookID) {
+      const path = `http://localhost:5000/books/${bookID}`;
+      axios
+        .delete(path)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book removed!';
+          this.showMessage = true;
+        })
+        .catch(error => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    onDeleteBook(book) {
+      this.removeBook(book.id);
     },
   },
   created() {
